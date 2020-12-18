6137efd3fae26f8abee197025fcea301cfb3bf2b
Abhinav Gangrade <abhinavgangrade0606@gmail.com>
2020-12-18 18:43:25 +0530

    Frontend Part Completed with Timer based Quiz and all actions

--- QAT/QAT/QAT_Final/views.py
+++ QAT/QAT/QAT_Final/views.py
@@ -87,41 +87,82 @@ def logout(request):
         return HttpResponseRedirect("/")
     else:
         return HttpResponseRedirect("/")
+x=""
 @login_required(login_url="/login")
 def subject1(request):
+    global x
+    y=""
     name = str(request.user.get_full_name())
     if str(request.user)=="QAT":
         lgt(request)
         return HttpResponseRedirect("/")
-    return render(request,"Subject1.html",{"name":name})
+    if request.method=="POST":
+        o1=request.POST.get("o1","default")
+        o2=request.POST.get("o2","default")
+        x="True"
+        print("Yes")
+    return render(request,"Subject1.html",{"name":name,"x":x,"y":y})
+a=""
+
 @login_required(login_url="/login")
 def subject2(request):
+    global a
+    y = ""
     name = str(request.user.get_full_name())
-    if str(request.user)=="QAT":
+    if str(request.user) == "QAT":
         lgt(request)
         return HttpResponseRedirect("/")
-    return render(request,"Subject2.html",{"name":name})
+    if request.method == "POST":
+        o1 = request.POST.get("o1", "default")
+        o2 = request.POST.get("o2", "default")
+        a = "True"
+        print("Yes")
+    return render(request, "Subject2.html", {"name": name, "x": a, "y": y})
+b=""
 @login_required(login_url="/login")
 def subject3(request):
+    global b
+    y = ""
     name = str(request.user.get_full_name())
-    if str(request.user)=="QAT":
+    if str(request.user) == "QAT":
         lgt(request)
         return HttpResponseRedirect("/")
-    return render(request,"Subject3.html",{"name":name})
+    if request.method == "POST":
+        o1 = request.POST.get("o1", "default")
+        o2 = request.POST.get("o2", "default")
+        b = "True"
+        print("Yes")
+    return render(request, "Subject3.html", {"name": name, "x": b, "y": y})
+c=""
 @login_required(login_url="/login")
 def subject4(request):
+    global c
+    y = ""
     name = str(request.user.get_full_name())
-    if str(request.user)=="QAT":
+    if str(request.user) == "QAT":
         lgt(request)
         return HttpResponseRedirect("/")
-    return render(request,"Subject4.html",{"name":name})
+    if request.method == "POST":
+        o1 = request.POST.get("o1", "default")
+        o2 = request.POST.get("o2", "default")
+        c = "True"
+        print("Yes")
+    return render(request, "Subject4.html", {"name": name, "x": c, "y": y})
+d=""
 @login_required(login_url="/login")
 def subject5(request):
+    global d
+    y = ""
     name = str(request.user.get_full_name())
-    if str(request.user)=="QAT":
+    if str(request.user) == "QAT":
         lgt(request)
         return HttpResponseRedirect("/")
-    return render(request,"Subject5.html",{"name":name})
+    if request.method == "POST":
+        o1 = request.POST.get("o1", "default")
+        o2 = request.POST.get("o2", "default")
+        d = "True"
+        print("Yes")
+    return render(request, "Subject5.html", {"name": name, "x": d, "y": y})
 @login_required(login_url="/login")
 def result(request):
     name = str(request.user.get_full_name())
@@ -129,6 +170,7 @@ def result(request):
         lgt(request)
         return HttpResponseRedirect("/")
     return render(request,"Result.html",{"name":name})
+# added
 @login_required(login_url="/login")
 def analysis(request):
     name = str(request.user.get_full_name())
